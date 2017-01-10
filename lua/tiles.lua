99a150a264947d4b13b9ab670fd7046d2d046773
paulWatt526 <necro526>
2017-01-10 13:45:55 +0800

    Updated tileset.

--- lua/tiles.lua
+++ lua/tiles.lua
@@ -1,7 +1,7 @@
 --
 -- created with TexturePacker (http://www.codeandweb.com/texturepacker)
 --
--- $TexturePacker:SmartUpdate:555151070cb168cc4515f97c9e704cc8:353141410d475a66d0ab1e5d995ca57e:f4492607ea55a754477543692c89a688$
+-- $TexturePacker:SmartUpdate:82e7fdcf46f55335e0f1d3d1c8f7a2cf:584532efb031556035292817a3f01ab9:f4492607ea55a754477543692c89a688$
 --
 -- local sheetInfo = require("mysheet")
 -- local myImageSheet = graphics.newImageSheet( "mysheet.png", sheetInfo:getSheet() )
@@ -38,9 +38,17 @@ SheetInfo.sheet =
             height=32,
 
         },
+        {
+            -- tiles_3
+            x=103,
+            y=1,
+            width=32,
+            height=32,
+
+        },
     },
     
-    sheetContentWidth = 102,
+    sheetContentWidth = 136,
     sheetContentHeight = 34
 }
 
@@ -50,6 +58,7 @@ SheetInfo.frameIndex =
     ["tiles_0"] = 1,
     ["tiles_1"] = 2,
     ["tiles_2"] = 3,
+    ["tiles_3"] = 4,
 }
 
 function SheetInfo:getSheet()
